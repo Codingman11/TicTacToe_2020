95559775f6639eed60706ef5bf1df7cf691c98d0
JessePel <39877962+Codingman11@users.noreply.github.com>
2020-05-27 22:26:23 +0300

    Assignment 1

--- src/temple.js
+++ src/temple.js
@@ -1,87 +1,78 @@
 import "./styles.css";
 
-let turn = 0,
-  EMPTY = "",
-  moves = 0,
-  MAX = 5;
-
 var pla1 = "X",
   pla2 = "O";
-var board = new Array(5).fill("");
 
-function makeArray(board) {
-  for (let i = 0; i < MAX; i++) {
-    board[i] = new Array(5).fill("");
-  }
+let moves = 0;
+
+//Creating array
+function createArray() {
+  let array = [];
+
+  return array;
 }
 
-if (document.readyState !== "loading") {
-  initialzeCode();
-}
-function initialzeCode() {
-  console.log("Initializing");
-  createTable();
-  makeArray(board);
-  console.log(board);
-}
+function addTable(array) {
+  let x = 0;
 
-function createTable() {
-  let table = document.createElement("table");
-  let a = 0;
-  table.setAttribute("id", "table");
-  for (let i = 0; i < MAX; i++) {
-    let row = document.createElement("tr");
-    for (let j = 0; j < MAX; j++) {
-      let cell = document.createElement("td");
-      cell.setAttribute("id", a);
+  let cTable = document.createElement("table");
+  cTable.setAttribute("id", "table");
+  for (let i = 0; i < 5; i++) {
+    let addTr = document.createElement("tr");
 
-      cell.addEventListener("click", set, false);
-      row.appendChild(cell);
-      a++;
+    for (let j = 0; j < 5; j++) {
+      let addTd = document.createElement("td");
+      addTd.setAttribute("id", x);
+      addTd.setAttribute("class", "cell");
+
+      array[x] = x.toString();
+      let node = document.createTextNode("");
+      addTd.appendChild(node);
+      addTr.appendChild(addTd);
+      x++;
     }
-    table.appendChild(row);
 
+    cTable.appendChild(addTr);
     let tab = document.getElementById("board");
-    tab.appendChild(table);
+    tab.appendChild(cTable);
   }
+  return array;
 }
 
-function set(event) {
-  if (this.innerHTML === EMPTY && turn === 0) {
-    this.innerHTML = pla1;
-    this.classList.add("x");
-    turn = 1;
-    moves++;
-    board[event.target.parentNode.rowIndex][event.target.cellIndex] = pla1;
-  } else if (this.innerHTML === EMPTY && turn === 1) {
-    this.innerHTML = pla2;
-    this.classList.add("o");
-    turn = 0;
-    moves++;
-    board[event.target.parentNode.rowIndex][event.target.cellIndex] = pla2;
-  } else {
-    alert("Already marked! Try again");
-  }
-  console.log(board);
-  if (winCondition()) {
-    alert("Winner");
-  }
-}
+function test(array) {
+  let cells = document.querySelectorAll(".cell");
+  let turn = 0;
 
-function winCondition() {
-  //Check row
-  let move;
-  for (let row = 0; row < MAX; row++) {
-    move = 0;
-    for (let col = 0; col < MAX; col++) {
-      if (board[row][col] === EMPTY) {
-        continue;
-      } else if (board[row][col] === board[row][col + 1]) {
-        move++;
-        if (move === 4) {
-          return true;
-        }
-      }
+  var getId = function() {
+    var el = this;
+
+    let arId = parseInt(el.id, 10);
+    console.log(arId);
+
+    if (turn === 0) {
+      el.classList.add("x");
+      el.innerHTML = pla1;
+      turn = 1;
+      array[arId] = pla1;
+    } else if (turn === 1) {
+      el.classList.add("o");
+      el.innerHTML = pla2;
+      turn = 0;
+      array[arId] = pla2;
     }
+
+    moves++;
+    console.log("Moves: " + moves);
+  };
+
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", getId);
   }
 }
+
+function init() {
+  let array = createArray();
+  array = addTable(array);
+  test(array);
+}
+init();
